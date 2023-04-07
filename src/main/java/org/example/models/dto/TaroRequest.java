52e9fe99dced880da23051abc79ea46e296f9433
ksushagolubeffa <90283484+ksushagolubeffa@users.noreply.github.com>
2023-04-07 15:09:53 +0300

    added project

---
+++ src/main/java/org/example/models/dto/TaroRequest.java
@@ -0,0 +1,18 @@
+package org.example.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+public class TaroRequest {
+
+    /** @serial oh_my_sweet_taro
+     * @author terenin
+     * @version 1.0
+     * @apiNote the realisation of request-ent are useless here but it stayed for a future version
+     * */
+
+}
---
+++ src/main/java/org/example/models/dto/zodiac/FriendLoveMatcher.java
@@ -0,0 +1,19 @@
+package org.example.models.dto.zodiac;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class FriendLoveMatcher {
+
+    private String zodiac;
+    private Integer val1;
+    private Integer val2;
+
+}
